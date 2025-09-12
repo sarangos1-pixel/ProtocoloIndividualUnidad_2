d0b4af7ab5da2b3dff337caadc671a73d77390cd
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-12 16:39:35 -0500

    taller 7: ejercicio 2 Prueba CuentaBancaria

---
+++ src/protocoloindividual_2/ClasePruebaCuentaBancaria.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package protocoloindividual_2;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ClasePruebaCuentaBancaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria("157345834", 5740, "Ahorros");
+        cuenta.mostrarDetalles();
+        
+    }
+}
+    
+    
+
