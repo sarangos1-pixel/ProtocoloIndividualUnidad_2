a2d9f34f700659f22770dfd12cc518da87561ad5
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-12 17:15:43 -0500

    taller 7: ejercicio 3 prueba clase Utilidades

---
+++ src/protocoloindividual_2/ClasePruebaUtilidades.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package protocoloindividual_2;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class ClasePruebaUtilidades {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Suma: " + Utilidades.suma(8, 4));
+        System.out.println("Resta: " + Utilidades.resta(7, 5));
+        System.out.println("Multiplicación: " + Utilidades.mult(5, 5));
+        System.out.println("División: " + Utilidades.div(20, 4));
+    }
+    
+}
