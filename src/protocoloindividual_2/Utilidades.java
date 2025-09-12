5b83ef99de67db12a58be154ae39ed8264d35561
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-12 17:15:19 -0500

    taller 7: ejercicio 3 clase Utilidades

---
+++ src/protocoloindividual_2/Utilidades.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package protocoloindividual_2;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Utilidades {
+   public static int suma(int a, int b) {
+        return a + b;
+    }
+   
+   public static int resta(int a, int b) {
+        return a - b;
+    }
+   
+   public static int mult(int a, int b){
+       return a * b;
+   }
+   
+   public static double div(double a, double b){
+       return a / b;
+   }
+   
+    
+}
