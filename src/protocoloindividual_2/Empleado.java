90a28177d0976b4c73447a770d864827a8c2bc3b
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-12 14:53:18 -0500

    taller 7: ejercicio 1 clase empleado

---
+++ src/protocoloindividual_2/Empleado.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package protocoloindividual_2;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Empleado {
+    public String nombre;
+    private int salario;
+    
+    public Empleado(String nombre, int salario){
+        this.nombre = nombre;
+        this.setSalario(salario);
+    }
+    
+    public int getSalario(){
+        return salario;
+    }
+    
+    public void setSalario(int salario) {
+        if (salario <= 100){
+            this.salario = salario;
+        }else {
+            System.out.println("El salario no puede ser menor que que 100");
+        }
+    }
+    
+}    
+
+
