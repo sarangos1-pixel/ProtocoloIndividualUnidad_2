8b45525bdf701f0e32cd9bf5bf1977e63abdb4f2
Sebastian <Sebastian@DESKTOP-1IJS1U5>
2025-09-16 16:38:57 -0500

    corrigiendo

--- src/protocoloindividual_2/Empleado.java
+++ src/protocoloindividual_2/Empleado.java
@@ -14,7 +14,7 @@ public class Empleado {
     
     public Empleado(String nombre, int salario){
         this.nombre = nombre;
-        this.setSalario(salario);
+        this.salario  = salario;
     }
     
     public int getSalario(){
@@ -29,6 +29,10 @@ public class Empleado {
         }
     }
     
-}    
+}
 
-
+//Ejercicio 1: Clase Empleado
+//Crea una clase Empleado con las propiedades nombre y salario. Define:
+//1. La propiedad nombre debe ser pública.
+//2. Proporciona métodos get y set públicos para la propiedad salario, con validaciones en el método set.
+//3. Crea una clase de prueba que instancie objetos de Empleado y acceda a las propiedades.
